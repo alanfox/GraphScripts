b92811f0335829e943ee588d9f919a1bab76e6a7
alanfox <alan@foxbrady.org.uk>
2015-09-18 21:45:46 +0100

    update alpha centrality

--- graph_alpha_centrality.py
+++ graph_alpha_centrality.py
@@ -7,9 +7,11 @@ Created on Tue Jan 13 15:02:47 2015
 
 import networkx as NX
 from alpha_centrality import alpha_centrality
+
+graph_filter = True
         
 root_folder = ('C:/Users/af26/LarvalDispersalResults/' +
-            'polcoms1990/Run_1000_longlife/')
+            'polcoms1990to2000_climatology/Run_1000_baseline/')
 
 filename = (root_folder + 'Networkdata/GraphCompose/' +
             'graph_compose_latlon_alpha.graphml')
@@ -20,7 +22,22 @@ infile.close()
 
 H.remove_edges_from(H.selfloop_edges())
 
-print H.nodes(data = True)
+#print H.nodes(data = True)
+
+# code to filter by alpha_exogenous. Above 0.6 removes all sites without
+# observations (includes uncertain observations)
+
+if graph_filter:
+    G = NX.DiGraph()
+    for n,d in H.nodes_iter(data= True):
+        if d['alpha_exogenous'] >= 0.6:
+            G.add_node(n,d)
+    for u,v,d in H.out_edges_iter(data = True):
+        if u in G.nodes() and v in G.nodes():
+            G.add_edge(u,v,d)
+    H = G        
+
+#print H.nodes(data=True)        
 
 betweenness = alpha_centrality(NX.reverse(H),0.2,evalue = 'alpha_exogenous')
 
@@ -28,13 +45,13 @@ print betweenness
 
 NX.set_node_attributes(H, 'alpha_centrality', betweenness)
 
-print H.nodes(data=True)
+#print H.nodes(data=True)
 
 #outfile = open(root_folder + 'Networkdata/GraphCompose/' +
 #            'graph_compose_latlon_percolation_centrality.graphml','w')
 #NX.write_graphml(H,outfile)
 #outfile.close()
 outfile = open(root_folder + 'Networkdata/GraphCompose/' +
-                'graph_compose_latlon_centralities.gexf','w')
+                'graph_compose_latlon_centralities_alpha0.1_filt.gexf','w')
 NX.write_gexf(H,outfile)
 outfile.close()
